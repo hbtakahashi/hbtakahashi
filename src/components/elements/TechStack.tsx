a7610bf03a364712614f84dd26b904782ad594a8
Kenta Takahashi <guilty.gill.tea@gmail.com>
2026-04-20 21:39:09 +0900

    update: 技術スタックのCore Tech部分の作成

--- src/components/elements/TechStack.tsx
+++ src/components/elements/TechStack.tsx
@@ -11,7 +11,17 @@ export default function TechStack() {
             </h2>
             <div className="absolute left-0  -bottom-4 h-1 w-20 bg-[#6BDC96]"></div>
           </div>
-          <h3 className="py-12 text-white">Core Tech (メインで使用)</h3>
+          <div className="flex py-12 gap-2.5">
+            <h3 className=" text-[#DDE2F3] text-xl font-bold tracking-tight">
+              Core Tech
+            </h3>
+            <p className="text-[#C6C6CC] text-sm my-auto tracking-tight font-medium">
+              (メインで使用)
+            </p>
+            <p className="text-[#6BDC96] tracking-wide text-[10px] bg-[#6BDC96]/20 border-[#6BDC96]/30 border py-0.5 px-3 my-auto">
+              熟練度：高
+            </p>
+          </div>
         </div>
       </section>
     </>
