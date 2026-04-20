377a5da737eed0091edebc0333be94675fb21665
Kenta Takahashi <guilty.gill.tea@gmail.com>
2026-04-20 21:27:07 +0900

    update: TechStackセクションの追加

---
+++ src/components/elements/TechStack.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function TechStack() {
+  return (
+    <>
+      <section>
+        <div className="w-full py-20 px-8 bg-[#161C27]">
+          <div className="relative inline-block">
+            <h2 className="text-lg text-[#828796] font-medium tracking-widest">
+              技術スタック
+            </h2>
+            <div className="absolute left-0  -bottom-4 h-1 w-20 bg-[#6BDC96]"></div>
+          </div>
+          <h3 className="py-12 text-white">Core Tech (メインで使用)</h3>
+        </div>
+      </section>
+    </>
+  );
+}
