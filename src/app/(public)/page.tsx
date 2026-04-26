11d4843981be7576a503e1e29607cd61ef038225
Kenta Takahashi <guilty.gill.tea@gmail.com>
2026-04-26 17:32:09 +0900

    feat: microCMSの導入

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,13 @@
 import Hero from "@/src/components/elements/Hero";
 import TechStack from "../../components/elements/TechStack";
+import { client } from "@/src/lib/client";
+import { Blog } from "@/src/types/blog";
 
-export default function Home() {
+export default async function Home() {
+  // コンポーネント内でデータを取得するように変更
+  const data = await client.get({ endpoint: "blog" });
+  console.log(data);
+
   return (
     <main>
       <Hero />
